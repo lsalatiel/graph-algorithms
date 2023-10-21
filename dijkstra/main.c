9938e1a437862a48e9c28ec272a6f945e418d994
lsalatiel <lucassalatiel1004@gmail.com>
2023-10-21 19:30:56 -0300

    update main

--- dijkstra/main.c
+++ dijkstra/main.c
@@ -6,19 +6,28 @@
 
 int main() {
     Graph *g = graph_construct(5);
-    
-    graph_add_directed_edge(g, 0, 1, 1.0);
-    graph_add_directed_edge(g, 0, 2, 2.0);
-    graph_add_directed_edge(g, 1, 3, 3.0);
-    graph_add_directed_edge(g, 2, 3, 4.0);
-    graph_add_directed_edge(g, 3, 4, 5.0);
+
+    graph_add_directed_edge(g, 0, 1, 10);
+    graph_add_directed_edge(g, 0, 2, 3);
+    graph_add_directed_edge(g, 2, 1, 4);
+    graph_add_directed_edge(g, 1, 2, 1);
+    graph_add_directed_edge(g, 1, 3, 2);
+    graph_add_directed_edge(g, 2, 3, 8);
+    graph_add_directed_edge(g, 2, 4, 2);
+    graph_add_directed_edge(g, 3, 4, 7);
+    graph_add_directed_edge(g, 4, 3, 9);
 
     graph_print(g);
 
-    int *path = dijkstra_algorithm(g, 0, 4);
+    int *path = dijkstra_algorithm(g, 0, 1);
 
-    for(int i = 0; i < 5; i++)
-        printf("%d ", path[i]);
+    for(int i = 0; i < 5; i++) {
+        if(path[i] == -1) {
+            printf("NULL\n");
+            break;
+        }
+        printf("%d -> ", path[i]);
+    }
     
     free(path);
     graph_destroy(g);
