1c3429a19d382a276f7c52e27d2c33a8d0a5708b
lsalatiel <lucassalatiel1004@gmail.com>
2023-10-21 19:31:08 -0300

    update dijkstra

--- dijkstra/dijkstra.c
+++ dijkstra/dijkstra.c
@@ -4,12 +4,13 @@
 #include "graph/linked_list.h"
 #include "priority_queue/heap.h"
 #include <stdlib.h>
-#include "math.h"
+#include <math.h>
 
-void __dijkstra_relax(Heap *h, double u_w, int v, double w) {
+void __dijkstra_relax(Heap *h, double *dist, double u_w, int v, double w) {
     double new_priority = u_w + w;
     if(heap_get_priority(h, v) > new_priority) {
         heap_decrease_key(h, v, new_priority);
+        dist[v] = new_priority;
     }
 }
 
@@ -19,9 +20,11 @@ int *dijkstra_algorithm(Graph *g, int s, int t) {
     int path_size = 0;
 
     Heap *h = heap_init(graph_num_vertices(g));
+    double *dist = malloc(sizeof(double) * graph_num_vertices(g));
 
     for(int i = 0; i < graph_num_vertices(g); i++) {
         heap_insert(h, i, i == s ? 0.0 : INFINITY);
+        dist[i] = i == s ? 0 : INFINITY;
     }
 
     LinkedList **l = graph_adjacency_list(g);
@@ -31,7 +34,7 @@ int *dijkstra_algorithm(Graph *g, int s, int t) {
         double u_priority = heap_get_priority(h, u);
         heap_pop(h);
         path[path_size++] = u;
-
+        
         if(u == t) break;
 
         ListIterator *it = list_iterator_construct(l[u]);
@@ -39,13 +42,15 @@ int *dijkstra_algorithm(Graph *g, int s, int t) {
         while(!list_iterator_is_over(it)) {
             int v = list_iterator_next(it);
             double w = graph_get_weight(g, u, v);
-            __dijkstra_relax(h, u_priority, v, w);
+            __dijkstra_relax(h, dist, u_priority, v, w);
         }
 
         list_iterator_free(it);
     }
 
     heap_destroy(h);
+
+    printf("Cost: %lf\n", dist[t]);
 
     return path;
 }
